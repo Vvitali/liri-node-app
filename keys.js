6581949c5c36f1dbc6731038ca2b3b1930df4f6f
Vitaliy Vorobyev <vvitali26@gmail.com>
2017-11-29 00:38:36 -0500

    twitter part is done

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('keys are loaded');
 
 var twitterKeys = {
-	consumer_key: '<input here>',
-	consumer_secret: '<input here>',
-	access_token_key: '<input here>',
-	access_token_secret: '<input here>',
+	consumer_key: '6QlgQSnQDEYwnMo6RDNTFkCxD',
+	consumer_secret: 'RndcLjKOdl5B8v1Pi7FiPPXFoL2zlSFxMNfJRJjwoMuMYlKP1Q',
+	access_token_key: '320257342-IflHDYYIWjRMiaXcWBG7KEjTbMtJphbIV9shi8l0',
+	access_token_secret: 'KLbZkPKGs5d3ddCTHRwet0hQUlGbIbvVd5odnN969PKcj',
 }
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,19 +1,41 @@
-console.log("Liri: start!");
+var DEBUG = false;
+
+DEBUG && console.log("Liri: start!");
 var twitterKeys = require("./keys.js")
+var Twitter = require('twitter');
+var client = Twitter(twitterKeys);
 
 switch(process.argv[2]){
 	case 'my-tweets':
-	
+	var params = {screen_name: 'Vitali_LoGoS4', 
+	count:20};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		error && console.log(error);
+		DEBUG && console.log(tweets);
+		for (var i in tweets){
+			console.log("▼▼ -- ▼▼")
+			console.log("Date: "+tweets[i].created_at)
+			console.log("Message: "+ tweets[i].text);
+		}
+	});
+
 	console.log(process.argv[2]);
 	break;
-	case 'spotify':
-	
+	case 'spotify-this-song':
+	request
 	console.log(process.argv[2]);
 	break;
-	case "movie-this":
-	
+	case "movie-this":	
+	console.log(process.argv[2]);
+	break;
+	case "do-what-it-says":
 	console.log(process.argv[2]);
 	break;
 	default:
 	break;
 }
+
+
+// request('http://www.google.com', function (error, response, body){
+
+// });
