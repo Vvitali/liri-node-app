65b931939822a10626dce08c8bac47c82512bf4a
Vitaliy Vorobyev <vvitali26@gmail.com>
2017-11-29 02:16:29 -0500

    the begging of spotify part

--- liri.js
+++ liri.js
@@ -5,6 +5,13 @@ var twitterKeys = require("./keys.js")
 var Twitter = require('twitter');
 var client = Twitter(twitterKeys);
 
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify({
+	id: "d028e5930377471885981d046250476a",
+	secret: "8142e3cf6e9a45bfbfed6e55944fb638"
+});
+
 switch(process.argv[2]){
 	case 'my-tweets':
 	var params = {screen_name: 'Vitali_LoGoS4', 
@@ -15,14 +22,23 @@ switch(process.argv[2]){
 		for (var i in tweets){
 			console.log("▼▼ -- ▼▼")
 			console.log("Date: "+tweets[i].created_at)
-			console.log("Message: "+ tweets[i].text);
+			//console.log("Message: "+ tweets[i].text);
 		}
 	});
-
 	console.log(process.argv[2]);
 	break;
 	case 'spotify-this-song':
-	request
+	spotify.search({ type: 'track', query: process.argv[3], limit: 3 }, function(error, data) {
+		error && console.log(error);
+		console.log("Artists:");
+		for (var i in data.tracks.items[0].artists){
+			console.log(data.tracks.items[0].artists[i].name)
+		}
+		console.log("The song's name: "+data.tracks.items[0].name);
+		// A preview link of the song from Spotify
+		// The album that the song is from
+	});
+
 	console.log(process.argv[2]);
 	break;
 	case "movie-this":	
